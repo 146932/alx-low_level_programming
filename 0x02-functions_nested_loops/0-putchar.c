1838736a70190778ba2ff2f4455f968f77fd7559
Gbadebotemitope77@yahoo.com <Gbadebotemitope77@yahoo.com>
2022-09-13 09:27:34 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "temitope.h"
+/**
+  * main - Prints Temitope
+  *
+  * Return: Always 0 (Success)
+  */
+ int main(void)
+ {
+         _putchar('T');
+         _purchar('e');
+	 _purchar('m');
+	 _purchar('i');
+	 _purchar('t');
+	 _purchar('o');
+	 _purchar('p');
+	 _purchar('e');
+	 _purchar('\n');
+	 return(0);
+}
