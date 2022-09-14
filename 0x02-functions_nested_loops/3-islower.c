2242f388acfcc05da3d969a5c5c4710671215ed6
Temitope Gbadebo <Gbadebotemitope77@yahoo.com>
2022-09-14 06:46:40 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,16 +2,20 @@
 
 /**
   * _islower - Checks for lowercase character
-  * @c: The character to be checked
+  * @r: The character to be checked
   *
-  * Return: 1 for lowercase character or 0 for anything else
+  * Return: 1 for lowercase character or 0 for otherwise
   */
-int _islower(int c)
+int _islower(int r)
 {
-	if (c >= 97 && c <= 122)
-	{
-		return (1);
-	}
+	int r;
 
-	return (0);
+    r = _islower('H');
+    _putchar(r + '0');
+    r = _islower('o');
+    _putchar(r + '0');
+    r = _islower(108);
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
 }
